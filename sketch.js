0b5d71e990f9ae6bdebb76a784727af76e518d10
Tulika1234 <tulikaghosh.1309@gmail.com>
2020-10-18 20:55:33 +0530

    the changes are commited

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint = Matter.Constraint
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var chain
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,7 +37,7 @@ function setup(){
     log6 = new Log(200,100,100,PI/2);
 
     bird = new Bird(100,100);
-
+    chain= new Chain(bird.body, log6.body);
 }
 
 function draw(){
@@ -60,7 +61,7 @@ function draw(){
     log4.display();
     log5.display();
     log6.display();
-
+    chain.display();
     bird.display();
     platform.display();
 }
